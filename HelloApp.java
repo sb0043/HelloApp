81640a38f1b152c4a31542fd8b08bae506a8966e
sb0043 <sb0043@srmist.edu.in>
2026-03-17 21:12:33 +0530

    UC5:to print hello world using string

--- HelloApp.java
+++ HelloApp.java
@@ -1,42 +1,26 @@
-/**
- * HelloAppUC4.java - A simple Java application that greets multiple users by name
- * if provided as command-line arguments, or defaults to greeting "World" if none
- * are given.
- *
- * Greet Multiple Users - The application should accept multiple names as
- * command-line arguments and display a personalized greeting for each user.
- *
- * Usage: java HelloAppUC4 [name1] [name2] ... [nameN]
- * - If names are provided, it will display "Hello, [Name1], [Name2], ..."
- * - If no names are provided, it will display "Hello, World!"
- *
- * @author Developer Name
- * @version 4.0
- * @since UC1
- */
-
 public class HelloApp {
 
     public static void main(String[] args) {
 
-        String name = "World";
-
-        // If names are provided, combine them
-        if (args.length > 0) {
-
-            StringBuilder nameBuilder = new StringBuilder();
-
-            for (int i = 0; i < args.length; i++) {
-                nameBuilder.append(args[i]);
-
-                if (i < args.length - 1) {
-                    nameBuilder.append(", ");
-                }
-            }
-
-            name = nameBuilder.toString();
+        // If no arguments are provided
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+            return;
         }
 
-        System.out.println("Hello, " + name + "!");
+        // Build names using StringBuilder
+        StringBuilder nameBuilder = new StringBuilder();
+        boolean first = true;
+
+        for (String name : args) {
+            if (!first) {
+                nameBuilder.append(", ");
+            }
+            nameBuilder.append(name);
+            first = false;
+        }
+
+        // Print final greeting
+        System.out.println("Hello, " + nameBuilder.toString() + "!");
     }
 }
